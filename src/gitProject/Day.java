1d200eb0741e21566cf4ed7b0f6f5447a453d321
Zingsun <Zingsun@Zingsun>
2017-02-19 18:45:37 +0800

    多线程

--- src/gitProject/Day.java
+++ src/gitProject/Day.java
@@ -1,7 +1,24 @@
 package gitProject;
 
-public class Day {
-	public static void main(String[] args) {
+public class Day implements Runnable{
+	public static int tikets=100;
+	String key=new String("key");
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while(true){
+			synchronized(key){
+				if(tikets>0){
+					System.out.println("第"+Thread.currentThread().getName()+"个车站这在卖第"+(101-tikets)+"张车票");
+					--tikets;
+					
+				}else{
+					break;
+				}
+			}
+		}
 		
 	}
+	
 }
--- src/gitProject/Month.java
+++ src/gitProject/Month.java
@@ -1,5 +1,14 @@
 package gitProject;
 
 public class Month {
-
+	public static void main(String[] args) {
+		Day day=new Day();
+		Thread th=new Thread(day);
+		Thread read=new Thread(day);
+		Thread thread=new Thread(day);
+		//thread.setPriority(Thread.NORM_PRIORITY +5);
+		read.start();
+		th.start();
+		thread.start();
+	}
 }
