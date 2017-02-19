9cad1601aab716dbc86766bcf4d7c750e1507c0e
Zingsun <Zingsun@Zingsun>
2017-02-19 19:48:11 +0800

    Revert "th更改为thr"
    This reverts commit cf71a174625eb1633aec4d57e11a5a16d175b22b.

--- src/gitProject/Month.java
+++ src/gitProject/Month.java
@@ -3,12 +3,12 @@ package gitProject;
 public class Month {
 	public static void main(String[] args) {
 		Day day=new Day();
-		Thread thr=new Thread(day);
+		Thread th=new Thread(day);
 		Thread read=new Thread(day);
 		Thread thread=new Thread(day);
 		//thread.setPriority(Thread.NORM_PRIORITY +5);
 		read.start();
-		thr.start();
+		th.start();
 		thread.start();
 	}
 }
