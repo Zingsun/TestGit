cf71a174625eb1633aec4d57e11a5a16d175b22b
Zingsun <Zingsun@Zingsun>
2017-02-19 18:52:20 +0800

    th更改为thr

--- src/gitProject/Month.java
+++ src/gitProject/Month.java
@@ -3,12 +3,12 @@ package gitProject;
 public class Month {
 	public static void main(String[] args) {
 		Day day=new Day();
-		Thread th=new Thread(day);
+		Thread thr=new Thread(day);
 		Thread read=new Thread(day);
 		Thread thread=new Thread(day);
 		//thread.setPriority(Thread.NORM_PRIORITY +5);
 		read.start();
-		th.start();
+		thr.start();
 		thread.start();
 	}
 }
