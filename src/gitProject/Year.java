45a940f658abc3d1bfb94f7d1a95a049424a85ad
Zingsun <Zingsun@Zingsun>
2017-02-19 19:10:51 +0800

    编写Calendar

--- src/gitProject/Year.java
+++ src/gitProject/Year.java
@@ -1,7 +1,17 @@
 package gitProject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Year {
-	public static void main(String[] args) {
-		System.out.println("successful!");
+	public static void main(String[] args) throws ParseException {
+		Calendar cal=Calendar.getInstance();
+		/*SimpleDateFormat sim=new SimpleDateFormat("yyyy-MM-dd");
+		Date parse = sim.parse("2017-02-19");*/
+		cal.setTime(new Date());
+		int day=cal.get(Calendar.DAY_OF_WEEK);
+		System.out.println(day);
 	}
 }
